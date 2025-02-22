d0f8d890801b9d79bc3ca7c0a71ed38d3e3619da
Nikola Ganchev <nikolamihailovg@gmail.com>
2025-02-22 23:00:35 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <cstdlib>
+#include <iostream>
+#include <filesystem>
+
+#include "templater.h"
+
+int main(int argc, char* argv[])
+{
+}
---
+++ test/test-library.cpp
@@ -0,0 +1,4 @@
+#include "catch2/catch_all.hpp"
+#include "templater.h"
+
+#include <chrono>
