837f37c5eaa10b4cb33148047b38a4d2c1d4d8e6
Nikola Ganchev <nikolamihailovg@gmail.com>
2025-04-01 22:26:22 +0300

    Document text functions

--- src/text.h
+++ src/text.h
@@ -4,10 +4,36 @@
 
 namespace Templater::dynamic {
     namespace text {
+        /**
+         * @brief Escape a string from XML injection causing elements.
+         * By default only escapes the crucial XML injection characters: <, >, ", ', &.
+         * It can also escape unicode sequences if the escapeMultiByte argument is set to true.
+         * Unicode sequences can be unsafe in envirionments that do not support UTF-8 and can be used to create injections. 
+         * Unicode escaping escapes any unicode sequence to an XML/HTML entity. 
+         * Due to the runtime cost and overall low risk this is false by default.
+         * 
+         * @param str 
+         * @param escapeMultiByte Whether Unicode characters should be escaped
+         * @return std::string 
+         */
         std::string escape(const std::string& str, bool escapeMultiByte = false);
 
+
+        /**
+         * @brief For a given unicode codepoint returns the appropriate XML/HTML entity.
+         * 
+         * @param codepoint 
+         * @return std::string 
+         */
         std::string numericEntity(uint32_t codepoint);
     
+
+        /**
+         * @brief Get the Unicode codepoint for a sequence of unicode characters. If the sequence is invalid, returns 0. Does not modify the original pointer.
+         * 
+         * @param read 
+         * @return uint32_t The codepoint if the sequence is valid, otherwise 0.
+         */
         uint32_t getUnicodeCodepoint(const char* read);
     }
 }
