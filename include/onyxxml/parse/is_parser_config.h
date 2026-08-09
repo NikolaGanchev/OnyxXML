5b687aa6dc8db49907f0b769674739afe8593624
Nikola Ganchev <nikolamihailovg@gmail.com>
2026-08-09 15:59:12 +0300

    Isolate isParserConfig to namespace

--- include/onyxxml/parse/is_parser_config.h
+++ include/onyxxml/parse/is_parser_config.h
@@ -2,9 +2,11 @@
 
 #include <concepts>
 
+namespace onyx::dynamic::parser {
 template <typename T>
 concept isParserConfig = requires {
     { T::validate } -> std::same_as<const bool&>;
     { T::validateDuplicateAttributes } -> std::same_as<const bool&>;
     { T::maxAttributeCount } -> std::same_as<const size_t&>;
 };
+}  // namespace onyx::dynamic::parser
